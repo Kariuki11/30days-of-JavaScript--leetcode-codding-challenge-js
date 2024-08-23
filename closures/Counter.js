1eae65d532a2253b04500e1c475d03cf82c7b66b
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-24 01:33:50 +0300

    "Formatted code and examples in Counter.js"

--- closures/Counter.js
+++ closures/Counter.js
@@ -4,39 +4,39 @@
 
  
 
-Example 1:
+                        Example 1:
 
-Input: 
-n = 10 
-["call","call","call"]
-Output: [10,11,12]
-Explanation: 
-counter() = 10 // The first time counter() is called, it returns n.
-counter() = 11 // Returns 1 more than the previous time.
-counter() = 12 // Returns 1 more than the previous time.
-Example 2:
+                        Input: 
+                        n = 10 
+                        ["call","call","call"]
+                        Output: [10,11,12]
+                        Explanation: 
+                        counter() = 10 // The first time counter() is called, it returns n.
+                        counter() = 11 // Returns 1 more than the previous time.
+                        counter() = 12 // Returns 1 more than the previous time.
+                        Example 2:
 
-Input: 
-n = -2
-["call","call","call","call","call"]
-Output: [-2,-1,0,1,2]
-Explanation: counter() initially returns -2. Then increases after each sebsequent call.
- 
+                        Input: 
+                        n = -2
+                        ["call","call","call","call","call"]
+                        Output: [-2,-1,0,1,2]
+                        Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+                        
 
-Constraints:
+                        Constraints:
 
--1000 <= n <= 1000
-0 <= calls.length <= 1000
-calls[i] === "call"
+                        -1000 <= n <= 1000
+                        0 <= calls.length <= 1000
+                        calls[i] === "call"
 
 
 //ANSWER
-function createCounter(n) {
-    return function() {
-        return n++;
-    };
-}
+            function createCounter(n) {
+                return function() {
+                    return n++;
+                };
+            }
 
 
-const counter = createCounter(10);
-console.log(counter()); // 10
+            const counter = createCounter(10);
+            console.log(counter()); // 10
