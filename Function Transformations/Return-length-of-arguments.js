5dd6dfc589feaaec79b538cb8f8f33c19c16782b
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 01:40:12 +0300

    "Formatted code and comments in Return-length-of-arguments.js"

--- Function Transformations/Return-length-of-arguments.js
+++ Function Transformations/Return-length-of-arguments.js
@@ -3,25 +3,25 @@
 //Write a function argumentsLength that returns the count of arguments passed to it.
  
 
-Example 1:
+            Example 1:
 
-Input: args = [5]
-Output: 1
-Explanation:
-argumentsLength(5); // 1
+            Input: args = [5]
+            Output: 1
+            Explanation:
+            argumentsLength(5); // 1
 
-One value was passed to the function so it should return 1.
-Example 2:
+            One value was passed to the function so it should return 1.
+            Example 2:
 
-Input: args = [{}, null, "3"]
-Output: 3
-Explanation: 
-argumentsLength({}, null, "3"); // 3
+            Input: args = [{}, null, "3"]
+            Output: 3
+            Explanation: 
+            argumentsLength({}, null, "3"); // 3
 
-Three values were passed to the function so it should return 3.
- 
+            Three values were passed to the function so it should return 3.
+            
 
-Constraints:
+            Constraints:
 
-args is a valid JSON array
-0 <= args.length <= 100
+            args is a valid JSON array
+            0 <= args.length <= 100
