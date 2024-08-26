bf9ebedd21132cfda6cccba287a409459f15e2cf
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 01:12:26 +0300

    "Added function 'filter' to filter elements from an array."

--- Basic array Transformations/Filter-elements-from-array.js
+++ Basic array Transformations/Filter-elements-from-array.js
@@ -43,4 +43,13 @@
 
 //ANSWER
 
+function filter(arr, fn) {
+    const filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
 
