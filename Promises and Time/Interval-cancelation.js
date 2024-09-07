6713750d7e98226cba525d90581a4c2b54ab0641
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-07 14:47:48 +0300

    changes

--- Promises and Time/Interval-cancelation.js
+++ Promises and Time/Interval-cancelation.js
@@ -11,29 +11,29 @@ The function fn should be called with args immediately and then called again eve
 
 Example 1:
 
-Input: fn = (x) => x * 2, args = [4], t = 35
-Output: 
-[
-   {"time": 0, "returned": 8},
-   {"time": 35, "returned": 8},
-   {"time": 70, "returned": 8},
-   {"time": 105, "returned": 8},
-   {"time": 140, "returned": 8},
-   {"time": 175, "returned": 8}
-]
-Explanation: 
-const cancelTimeMs = 190;
-const cancelFn = cancellable((x) => x * 2, [4], 35);
-setTimeout(cancelFn, cancelTimeMs);
+                        Input: fn = (x) => x * 2, args = [4], t = 35
+                        Output: 
+                        [
+                        {"time": 0, "returned": 8},
+                        {"time": 35, "returned": 8},
+                        {"time": 70, "returned": 8},
+                        {"time": 105, "returned": 8},
+                        {"time": 140, "returned": 8},
+                        {"time": 175, "returned": 8}
+                        ]
+                        Explanation: 
+                        const cancelTimeMs = 190;
+                        const cancelFn = cancellable((x) => x * 2, [4], 35);
+                        setTimeout(cancelFn, cancelTimeMs);
 
-Every 35ms, fn(4) is called. Until t=190ms, then it is cancelled.
-1st fn call is at 0ms. fn(4) returns 8.
-2nd fn call is at 35ms. fn(4) returns 8.
-3rd fn call is at 70ms. fn(4) returns 8.
-4th fn call is at 105ms. fn(4) returns 8.
-5th fn call is at 140ms. fn(4) returns 8.
-6th fn call is at 175ms. fn(4) returns 8.
-Cancelled at 190ms
+                        Every 35ms, fn(4) is called. Until t=190ms, then it is cancelled.
+                        1st fn call is at 0ms. fn(4) returns 8.
+                        2nd fn call is at 35ms. fn(4) returns 8.
+                        3rd fn call is at 70ms. fn(4) returns 8.
+                        4th fn call is at 105ms. fn(4) returns 8.
+                        5th fn call is at 140ms. fn(4) returns 8.
+                        6th fn call is at 175ms. fn(4) returns 8.
+                        Cancelled at 190ms
 Example 2:
 
 Input: fn = (x1, x2) => (x1 * x2), args = [2, 5], t = 30
