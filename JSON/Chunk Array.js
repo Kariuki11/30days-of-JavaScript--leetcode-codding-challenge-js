6839eb1e3475c2053cac99e97ca57965c215d4dc
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-19 13:25:46 +0300

    changes

--- JSON/Chunk Array.js
+++ JSON/Chunk Array.js
@@ -44,3 +44,20 @@
                                             
 
                         //Answer
+
+                        function chunkArray(arr, size) {
+                            const chunkedArr = [];
+                            
+                            for (let i = 0; i < arr.length; i += size) {
+                                chunkedArr.push(arr.slice(i, i + size));
+                            }
+                            
+                            return chunkedArr;
+                        }
+                        
+                        // Example usage:
+                        console.log(chunkArray([1, 2, 3, 4, 5], 1));   // Output: [[1], [2], [3], [4], [5]]
+                        console.log(chunkArray([1, 9, 6, 3, 2], 3));  // Output: [[1, 9, 6], [3, 2]]
+                        console.log(chunkArray([8, 5, 3, 2, 6], 6));  // Output: [[8, 5, 3, 2, 6]]
+                        console.log(chunkArray([], 1));               // Output: []
+                        
